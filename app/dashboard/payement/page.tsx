0c9815e5caf524619254928b90b803bd8d2af45b
Derrickogouwole <81334223+wilfriedogouwole@users.noreply.github.com>
2024-11-03 10:16:25 +0100

    update

--- app/dashboard/payement/page.tsx
+++ app/dashboard/payement/page.tsx
@@ -8,33 +8,29 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-export default function PagePayement(){
+
+export default function PagePayement() {
   return (
-    <div className="mx-auto pb-10" >
-          <h1 className=" flex justify-center text-3xl font-semibold">Paiement</h1>
-        <div className="pt-8 flex justify-center items-start gap-6 md:grid-cols-[180px_1fr] lg:grid-cols-[250px_1fr]">
-         
-          <div className="flex flex-col gap-6 text-center  justify-center items-center">
-            <Card x-chunk="  dashboard-04-chunk-1">
-              <CardHeader>
-                <CardTitle>Offre actuelle</CardTitle>
-                <CardDescription>
-                  Montant de votre offre actuelle
-                </CardDescription>
-              </CardHeader>
-              <CardContent>
-             <p className="text-center">16euros</p>
-              </CardContent>
-              <CardFooter className="justify-center border-t px-6 py-4">
-                <Button>Changer d&#39;offre</Button>
-              </CardFooter>
-            </Card>
-
-            </div>
-           
-            
-          </div>
+    <div className="mx-auto pb-10">
+      <h1 className="flex justify-center text-3xl font-semibold">Paiement</h1>
+      <div className="pt-8 flex justify-center items-start gap-6 md:grid-cols-[180px_1fr] lg:grid-cols-[250px_1fr]">
+        <div className="flex flex-col gap-6 text-center justify-center items-center">
+          <Card x-chunk="dashboard-04-chunk-1">
+            <CardHeader>
+              <CardTitle>Offre actuelle</CardTitle>
+              <CardDescription>
+                Montant de votre offre actuelle
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p className="text-center">16 euros</p>
+            </CardContent>
+            <CardFooter className="justify-center border-t px-6 py-4">
+              <Button>Changer d&apos;offre</Button>
+            </CardFooter>
+          </Card>
         </div>
-  )
-
-  }
+      </div>
+    </div>
+  );
+}
