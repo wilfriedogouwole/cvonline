38c23b1a759450bbd214c5b3e8388c7ac22cb812
Derrickogouwole <81334223+wilfriedogouwole@users.noreply.github.com>
2024-11-04 16:55:09 +0100

    update

--- app/dashboard/editvc/page.tsx
+++ app/dashboard/editvc/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Cardmodel from '@/app/components/cardmodel'
+import Cardmodel from '@/components/Cardmodel'
 export default function page() {
   return (
     <div>
