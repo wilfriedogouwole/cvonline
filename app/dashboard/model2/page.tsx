aaba225305ff31cb35ec1e6c193e350cb0b65ea5
Derrickogouwole <81334223+wilfriedogouwole@users.noreply.github.com>
2024-11-08 17:18:10 +0100

    update model 2

--- app/dashboard/model2/page.tsx
+++ app/dashboard/model2/page.tsx
@@ -1,7 +1,8 @@
 "use client";
+import { Slider } from "@/components/ui/slider"
 import { Separator } from "@/components/ui/separator";
 import "@/styles/style.css";
-import { Accordion, AccordionItem, Button, Slider } from "@nextui-org/react";
+import { Accordion, AccordionItem, Button } from "@nextui-org/react";
 import { CirclePlus, Globe, Mail, MapPin, Phone } from "lucide-react";
 import dynamic from 'next/dynamic';
 import Image from "next/image";
@@ -12,17 +13,6 @@ import { useState } from 'react';
 const TextEditor = dynamic(() => import("@/components/TextEditor"), { ssr: false });
 
 export default function EditVC() {
-
-  const colors = [
-    "foreground",
-    "primary",
-    "secondary",
-    "success",
-    "warning",
-    "danger",
-  ];
-
-
   const [cvData, setCvData] = useState({
     photo: '',
     prenom: '',
@@ -484,23 +474,23 @@ PROFIL        </h2>
         <h3 className="text-sm font-semibold text-pink-700"> COMPETENCES</h3>
 
         <div className="p-3 bg-[#f9eee0] border border-pink-300 mt-[-17px] ">
-        <div className="flex flex-col gap-6 w-full max-w-md">
-      {colors.map((color) => (
-        <Slider
-          key={color}
-          color={color}
-          step={0.01}
-          maxValue={1}
-          minValue={0}
-          defaultValue={0.7}
-          aria-label="Temperature"
-          className="max-w-md"
-        />
-      ))}
-    </div>
 
-          <p className="text-sm font-semibold text-black">Contenu de la deuxième ligne de la deuxième colonne.</p>
-        </div>
+        <p className="text-sm font-semibold text-black pb-2">Comptétence 1</p>
+<p><Slider defaultValue={[0]} max={100} step={1} /></p> 
+<p className="text-sm font-semibold text-black pt-3">Comptétence 2 </p>
+
+<p className="pt-3"> <Slider defaultValue={[0]} max={100} step={1} /></p> 
+
+<p className="text-sm font-semibold text-black pt-3">Comptétence 3</p>
+<p><Slider defaultValue={[0]} max={100} step={1} /></p> 
+<p className="text-sm font-semibold text-black pt-3">Comptétence 4 </p>
+<p><Slider defaultValue={[0]} max={100} step={1} /></p> 
+
+<p className="text-sm font-semibold text-black pt-3">Comptétence 5</p>
+<p><Slider defaultValue={[0]} max={100} step={1} /></p> 
+<p className="text-sm font-semibold text-black pt-3">Comptétence 6 </p>
+
+<p className="pt-3"> <Slider defaultValue={[0]} max={100} step={1} /></p>        </div>
       </div>
     </section>
 
